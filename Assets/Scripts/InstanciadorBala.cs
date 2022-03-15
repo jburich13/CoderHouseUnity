897688d6d5b19d809bd5167d9731a6d20c4d3b8c
Juan Burich <burichojrs@gmail.com>
2022-03-15 11:26:06 -0300

    Ejercicio Disparos Temporizados

--- Assets/Scripts/InstanciadorBala.cs
+++ Assets/Scripts/InstanciadorBala.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,42 @@ public class InstanciadorBala : MonoBehaviour
 {
     [SerializeField] private GameObject bala;
     [SerializeField] private Transform spawner;
+    [SerializeField] private float tiempoParaInstanciar;
+    private float tiemporRestante;
     void Start()
     {
-        Instantiate(bala,spawner);
+        ResetTimer();
+        
+    }
+    void Update()
+    {
+        Temporizador();
+        AumentarTamaño();
+    }
+
+    void ResetTimer()
+    {
+        tiemporRestante = tiempoParaInstanciar;
+    }
+
+    void Temporizador()
+    {
+        tiemporRestante -= Time.deltaTime;
+        if (tiemporRestante <= 0)
+        {
+            var instanciaBala = Instantiate(bala,spawner.position, Quaternion.identity);
+            Destroy(instanciaBala,5f);
+            ResetTimer();
+            
+        }
+    }
+
+
+    void AumentarTamaño()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            bala.transform.localScale += bala.transform.localScale * 2;
+        }
     }
 }
